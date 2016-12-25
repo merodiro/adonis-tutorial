ff737a9aca3b21a303e681ef61bcd43760b0837b
Amr <merodiro@gmail.com>
2016-12-25 04:54:46 +0200

    adding Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node --harmony_proxies server.js
--- app/Http/Controllers/PostsController.js
+++ app/Http/Controllers/PostsController.js
@@ -6,41 +6,41 @@ const Validator = use('Validator')
 class PostsController {
 
     * index(request, response) {
-    	const posts = yield Post.query().orderBy('id', 'desc').fetch()
+        const posts = yield Post.query().orderBy('id', 'desc').fetch()
         yield response.sendView('home', { posts: posts.toJSON() })
     }
 
-    * create (request, response) {
-    	yield response.sendView('posts.create')
+    * create(request, response) {
+        yield response.sendView('posts.create')
     }
 
-    * store (request, response) {
-    	const postData = request.only('title', 'content')
+    * store(request, response) {
+        const postData = request.only('title', 'content')
 
-    	const rules = {
-    		title: 'required',
-    		content: 'required'
-    	}
+        const rules = {
+            title: 'required',
+            content: 'required'
+        }
 
-    	const validation = yield Validator.validate(postData, rules)
+        const validation = yield Validator.validate(postData, rules)
 
-    	if(validation.fails()) {
-    		yield request
-    			.withOnly('title', 'content')
-    			.andWith({ errors: validation.messages() })
-    			.flash()
+        if (validation.fails()) {
+            yield request
+                .withOnly('title', 'content')
+                .andWith({ errors: validation.messages() })
+                .flash()
 
-			response.redirect('back')
-			return
-    	}
+            response.redirect('back')
+            return
+        }
 
-    	yield Post.create(postData)
-    	response.redirect('/')
+        yield Post.create(postData)
+        response.redirect('/')
     }
 
-    *show (request, response) {
-    	const post = yield Post.find(request.param('id'))
-    	yield response.sendView('posts.show', { post: post.toJSON() })
+    * show(request, response) {
+        const post = yield Post.find(request.param('id'))
+        yield response.sendView('posts.show', { post: post.toJSON() })
     }
 }
 
